bfdc97d08252ffb10d367b1f569c0fc97d114f34
Toni Akinjiola <toni.akinjiola@STKIZ00318.local>
2019-05-13 11:34:50 +0200

    Changed conflicting type definition

--- index.d.ts
+++ index.d.ts
@@ -3,6 +3,6 @@
 import { useState, useEffect,  Dispatch, SetStateAction, MutableRefObject } from 'react'
 
 declare module 'react-load-more-hook' {
-  export type UseLoadMore = [boolean, Dispatch<SetStateAction<boolean>>]
-  export type useLoadMore = (callback: () => void, ref: MutableRefObject<HTMLElement>) => UseLoadMore
+  export type LoadMoreHook = [boolean, Dispatch<SetStateAction<boolean>>]
+  export type useLoadMore = (callback: () => void, ref: MutableRefObject<HTMLElement>) => LoadMoreHook
 }
